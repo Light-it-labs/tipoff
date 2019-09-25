123e7ead053c565ff5e6ede422d2abb82bf70d70
cjreysayago <rey933@gmail.com>
2019-09-25 17:32:37 -0300

    Added domains config file

---
+++ config/domains.example.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * This file stores all domains that are going to be used
+ *  Applying path aliasing for each of them
+ *
+ */
+
+return [
+    'domain' => [
+        'partials' => 'partials'
+    ]
+];
